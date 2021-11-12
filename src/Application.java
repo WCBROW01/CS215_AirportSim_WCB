f9f982104abd2b90d1a898456f9c1f8b658d33af
Will Brown <kyrosewoodgang@gmail.com>
2021-11-11 22:30:51 -0500

    [Application.java] Clean up code, add javadocs

--- src/Application.java
+++ src/Application.java
@@ -1,14 +1,24 @@
 import java.util.Timer;
 import java.util.TimerTask;
 
+/**
+ * Entrypoint for the airport simulator.
+ * @author Will Brown
+ * @version 1.0
+ * Fall 2021
+ */
 public class Application {
 
-	static final int TIMESTEP = 30;
 	static final int UPDATERATE = 1250;
-
+	static final int TIMESTEP   =   30;
+	
+	/**
+	 * Creates a timer that runs every UPDATERATE ms
+	 * and updates the airport as well as the screen.
+	 */
 	public static void main(String[] args) {
-		Timer airportTimer = new Timer();
 		Airport simAirport = new Airport();
+		Timer airportTimer = new Timer();
 		
 		TimerTask updateAirport = new TimerTask() {
 			public void run() {
