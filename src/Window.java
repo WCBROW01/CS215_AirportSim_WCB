0a232916697b7850c761681cad0933c19e3c5f18
Will Brown <kyrosewoodgang@gmail.com>
2021-11-18 21:53:30 -0500

    [Window.java] Add native theming

--- src/Window.java
+++ src/Window.java
@@ -6,6 +6,8 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class Window extends JFrame {
 
@@ -13,6 +15,15 @@ public class Window extends JFrame {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+	// Try to use the system theme.	
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+				| UnsupportedLookAndFeelException e1) {
+			// If this happens, we can't run the program anyways since we don't have a GUI.
+			e1.printStackTrace();
+		}
+	
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
